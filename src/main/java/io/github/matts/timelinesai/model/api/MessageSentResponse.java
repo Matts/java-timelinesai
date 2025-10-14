6094ae9122d1e6242e3af94219edb7701941b595
Matthew Smeets <matthew@reuzenpanda.nl>
2025-10-14 12:44:58 +0200

    0.1.3

--- src/main/java/io/github/matts/timelinesai/model/api/MessageSentResponse.java
+++ src/main/java/io/github/matts/timelinesai/model/api/MessageSentResponse.java
@@ -13,9 +13,16 @@ public class MessageSentResponse {
     private String status;
     private String message;
     private HashMap<String,String> data;
+    private String errors;
 
     public MessageSentResponse(String status, String message) {
         this.status = status;
         this.message = message;
+    }
+
+    public MessageSentResponse(String status, String message, HashMap<String,String> data) {
+        this.status = status;
+        this.message = message;
+        this.data = data;
     }
 }
